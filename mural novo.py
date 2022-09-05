98cd40a4f4ac29116c3246ac5b6a1611f2b3dfaa
RobsonaCosta <87870522+RobsonaCosta@users.noreply.github.com>
2022-09-05 20:51:52 -0300

    Update mural novo.py

--- mural novo.py
+++ mural novo.py
@@ -2,5 +2,5 @@ from time import sleep
 
 print("Felipe Paixão")
 print("Espero que eu já esteja para virar um cientista de dados!")
-print("Thais Faustino: espero estar formada em TI e trabalhando como programadora há um boom tempo")
+print ( "A Engenharia que  me aguarde!" )
 sleep(1)
